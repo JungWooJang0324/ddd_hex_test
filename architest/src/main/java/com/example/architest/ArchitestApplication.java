01fb46334515e61a85d656e69d5fc15cd5486b93
장정우 <chiang030204@gmail.com>
2024-05-16 17:47:41 +0900

    initial commit

---
+++ architest/src/main/java/com/example/architest/ArchitestApplication.java
@@ -0,0 +1,13 @@
+package com.example.architest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitestApplication.class, args);
+	}
+
+}
